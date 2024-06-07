01446ba4f7f726b1d24089dd33d21a0b13029a86
1 <rkdguswns99@gmail.com>
2024-06-07 15:06:10 +0900

    06_07

--- Assets/Scripts/CardBase.cs
+++ Assets/Scripts/CardBase.cs
@@ -2,13 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CardRarity: byte
+{
+    Nomal = 0,
+    Rare,
+    Unique,
+    Epic,
+}
+public enum CardType: byte
+{
+    Attack = 0,
+    defence,
+    posture,
+    technique,
+}
+
+
+
 [CreateAssetMenu(fileName = "NewCard", menuName = "Card Game/Card", order = 0)]
 public class CardBase : ScriptableObject
 {
-    public string CardName;
-    public string CardDecription;
-    public int CardCost;
-    public Sprite CardImage;
+    public string Name;
+    public string Decription;
+    public int Cost;
+    public CardRarity Rarity;
+    public CardType Type;
+    public Sprite Image;
 
     public virtual void Play(Player player, Enemy enemy)
     {
